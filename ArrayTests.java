5805de1e510ae868d9c1b804f4272e10edcb1e1c
moonwsy <114559407+moonwsy@users.noreply.github.com>
2022-10-14 20:29:35 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,23 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace1() {
+    int[] input1 = { 3,2,1 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 1,2,3 }, input1);
+	}
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void averageWithoutLowest() {
+    double[] input1 = {2.0,2.0,2.0};
+    assertEquals(2.0, ArrayExamples.averageWithoutLowest(input1), 0.01);
+  }
 }
