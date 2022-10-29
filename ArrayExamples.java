cd279ab86703077d8f0432dacfafd1d315678388
moonwsy <114559407+moonwsy@users.noreply.github.com>
2022-10-28 21:11:02 -0700

    Update ArrayExamples.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -17,7 +17,6 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      //arr[i] = newArray[arr.length - i - 1];
       newArray[i] = arr[arr.length - i - 1];
     }
     return newArray;
